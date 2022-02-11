bc1dacff446110a2f46feff0f9a84d16895de797
JM-2 <shinobi1009@hotmail.com>
2022-02-11 17:44:00 -0300

    commit inicial

---
+++ WebApiEf/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiEf.Data;
+using WebApiEf.Models;
+
+namespace WebApiEf.Controllers
+{
+    [ApiController]
+    [Route("v1/products")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).ToListAsync();
+            return products;
+        }
+
+        //[HttpGet]
+        //[Route("{id:int}")]
+        //public async Task<ActionResult<List<Product>>> GetById([FromServices] DataContext context, int id)
+        //{
+        //    var product = await context.Products.Include(x => x.Category)
+        //        .AsNoTracking()
+        //        .FirstOrDefaultAsync(x => x.Id == id);
+        //    return product;
+        //}
+    }
+}
---
+++ WebApiEf/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiEf.Models;
+
+namespace WebApiEf.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
